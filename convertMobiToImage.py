1b592a6c7c79baa71894df82885e0a09b0562ac5
Blind Holmes <hewenhan@gmail.com>
2022-08-22 22:06:54 +0800

    update

--- convertMobiToImage.py
+++ convertMobiToImage.py
@@ -1,6 +1,6 @@
 # pip3 install mobi
 import os
-import json, asyncio, time, datetime, sys, atexit, serial, shutil
+import time, shutil
 from multiprocessing import Pool, cpu_count
 
 inputDir = 'input'
